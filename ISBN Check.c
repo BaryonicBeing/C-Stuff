92536ff0b2a3237150562f9a4110345d204b0c0a
BaryonicBeing <klemm.maximilian@web.de>
2016-01-03 11:07:00 +0100

    Update ISBN Check.c
    added comments

--- ISBN Check.c
+++ ISBN Check.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-//sum = 272
-//388 229 192 3
-
+//this code just works for ISBN with length 10.
 int checkIsbn(char* isbn){
 	int i, sum = 0;
+	/*in the for loop 10*i is added to sum if it reads an X,
+	   otherwise it reads the current pointer value * i.*/
 	for (i = 10; i >= 1; --i)
 		sum += *isbn == 'X' ? 10 * i : (*isbn++-48) * i;
 	return sum % 11;
